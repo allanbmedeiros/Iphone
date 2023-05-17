f934242c8e2a05a80233c04351f8323d9470658b
allanbmedeiros <allan.bmedeiros@hotmail.com>
2023-05-17 19:43:15 -0300

    Primeiro Repostório

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background=cor
+}
+
+function trocaimg(imagem){
+    let iphone = document.querySelector(".iphone")
+    iphone.src=imagem
+}
